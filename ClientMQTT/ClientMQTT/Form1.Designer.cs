ac15b5f2c2a0b595201e6e2f26efd222850f25e1
thuantran <thuantq.uit@gmail.com>
2016-05-15 23:29:16 +0700

    sdsdsd

--- ClientMQTT/ClientMQTT/Form1.Designer.cs
+++ ClientMQTT/ClientMQTT/Form1.Designer.cs
@@ -41,11 +41,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(435, 29);
+            this.button1.Location = new System.Drawing.Point(292, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Gửi thông tin";
+            this.button1.Text = "gui GPS";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -65,11 +65,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(21, 109);
+            this.button2.Location = new System.Drawing.Point(80, 75);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "kenh2";
+            this.button2.Text = "gui nhiet do";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
--- ClientMQTT/ClientMQTT/Form1.cs
+++ ClientMQTT/ClientMQTT/Form1.cs
@@ -24,15 +24,16 @@ namespace ClientMQTT
         public Form1()
         {
             InitializeComponent();
-            client = new MqttClient(IPAddress.Parse("127.0.0.1"));
-            dcuSerial = "222AAA";
+            client = new MqttClient(IPAddress.Parse("45.117.80.39"));
+            //client = new MqttClient("http://45.117.80.39:1883");
+            dcuSerial = "2s22AAA";
             DPM = "DPM";
             client.Connect(dcuSerial);
             textBox1.Text = "connect thanh cong";
             CustomLog.LogError("onnect thanh cong");
-            string[] topic = { "#", "req/#" };
+            string[] topic = { "#", "test/#", "test" };
 
-            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
             client.Subscribe(topic, qosLevels);
 
             client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
@@ -45,14 +46,6 @@ namespace ClientMQTT
         {
             string result = "Nhan client_2: " + System.Text.Encoding.UTF8.GetString(e.Message);
             CustomLog.LogError(result);
-
-            byte[] abc = new byte[] { 0x01, 0x09, 0x31, 0x30, 0x2e, 0x38, 0x31, 0x37, 0x37, 0x33, 0x31 };
-            byte[] read = new byte[] { 0x01, 0x01, 0x01, 0x02, 0x04, 0x00, 0x00, 0x00, 0x01, 0x03, 0x01, 0x01, 0x24, 0x01, 0x03, 0x01, 0x01, 0x01 };
-            byte[] guilen = new byte[] { 0x02, 0x04, 0x00, 0x00, 0x00, 0x01, 0x05, 0x01, 0x01, 0x03, 0x04, 0x00, 0x00, 0x00, 0x01, 0x1F, 0x21, 0x18, 0x31, 0x30, 0x34, 0x39, 0x2E, 0x30, 0x36, 0x35, 0x31, 0x2C, 0x4E, 0x2C, 0x31, 0x30, 0x36, 0x33, 0x37, 0x2E, 0x32, 0x33, 0x30, 0x38, 0x2C, 0x45 };
-
-            string sdssdsdsdsds = Encoding.UTF8.GetString(read);
-            string sdsds = Encoding.UTF8.GetString(abc);
-            string sds = Encoding.UTF8.GetString(guilen);
         }
 
         void client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
@@ -68,30 +61,12 @@ namespace ClientMQTT
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            string topic = "info/" + dcuSerial + "/" + DPM;
-            MessageConfig result = new MessageConfig();
-            result.CommandType = "2";
-            result.CommandId = "1";
-            result.CommandAction = "Get";
-            result.Data = new Data()
-            {
-                Temperature = 35,
-                Longitude = 52858588,
-                Latitude = 58582525
-            };
-            result.Status = 1;
-
-            var javaScriptSerializer = new JavaScriptSerializer();
-            string message = javaScriptSerializer.Serialize(result);
-            CustomLog.LogError(message);
-            client.Publish(topic, Encoding.UTF8.GetBytes(message));
-            textBox1.Text = message;
+            client.Publish("toado", Encoding.UTF8.GetBytes(textBox3.Text));
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            client.Publish("test/" + dcuSerial + "/ds/sds", Encoding.UTF8.GetBytes("client2gui test/ds/sds Hello !!"));
-            textBox1.Text = "client2gui test/ds/sds Hello !!";
+            client.Publish("nhietdo", Encoding.UTF8.GetBytes(textBox2.Text));
         }
 
         private void abc()

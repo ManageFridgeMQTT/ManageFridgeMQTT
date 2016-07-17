f41992b571e32fa7e47353ca310a632632bb4ffe
thuantran <thuantq.uit@gmail.com>
2016-07-17 11:55:44 +0700

    update

--- WebManageFridgeMQTT/WebManageFridgeMQTT/Utility/CustomLog.cs
+++ WebManageFridgeMQTT/WebManageFridgeMQTT/Utility/CustomLog.cs
@@ -14,29 +14,32 @@ namespace WebManageFridgeMQTT.Utility
         private static object locker = new object();
         public static void LogArrayByte(byte[] mess)
         {
-            try
+            lock (locker)
             {
-                string hex = BitConverter.ToString(mess);
-                string text = hex.Replace("-", " ");
-
-                StringBuilder builder = new StringBuilder();
-                builder
-                    .AppendLine("----------")
-                    .AppendLine(DateTime.Now.ToString())
-                    .AppendFormat("Message:\t{0}", text)
-                    .AppendLine();
-
-                string filePath = CustomLog.LogPath + "Log.txt";
-                using (StreamWriter writer = File.AppendText(filePath))
+                try
                 {
-                    writer.Write(builder.ToString());
-                    writer.Flush();
+                    string hex = BitConverter.ToString(mess);
+                    string text = hex.Replace("-", " ");
+
+                    StringBuilder builder = new StringBuilder();
+                    builder
+                        .AppendLine("----------")
+                        .AppendLine(DateTime.Now.ToString())
+                        .AppendFormat("Message:\t{0}", text)
+                        .AppendLine();
+
+                    string filePath = CustomLog.LogPath + "Log.txt";
+                    using (StreamWriter writer = File.AppendText(filePath))
+                    {
+                        writer.Write(builder.ToString());
+                        writer.Flush();
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                CustomLog.LogError(ex);
-                throw;
+                catch (Exception ex)
+                {
+                    CustomLog.LogError(ex);
+                    throw;
+                }
             }
         }
         public static void LogError(Exception ex)
@@ -90,105 +93,115 @@ namespace WebManageFridgeMQTT.Utility
 
         public static void LogError(string filename, dynamic obj)
         {
-            StringBuilder builder = new StringBuilder();
-            builder
-                .AppendLine("----------")
-                .AppendLine(DateTime.Now.ToString())
-                .AppendFormat("To email:\t{0}", obj.Email)
-                .AppendLine()
-                .AppendFormat("Username:\t{0}", obj.UserName)
-                .AppendLine()
-                .AppendFormat("Password:\t{0}", obj.Password)
-                .AppendLine()
-                .AppendFormat("ConfirmationToken:\t{0}", obj.ConfirmationToken)
-                .AppendLine();
-
-            string filePath = CustomLog.LogPath + "Log.txt";
-            using (StreamWriter writer = File.AppendText(filePath))
+            lock (locker)
             {
-                writer.Write(builder.ToString());
-                writer.Flush();
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("To email:\t{0}", obj.Email)
+                    .AppendLine()
+                    .AppendFormat("Username:\t{0}", obj.UserName)
+                    .AppendLine()
+                    .AppendFormat("Password:\t{0}", obj.Password)
+                    .AppendLine()
+                    .AppendFormat("ConfirmationToken:\t{0}", obj.ConfirmationToken)
+                    .AppendLine();
+
+                string filePath = CustomLog.LogPath + "Log.txt";
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
             }
         }
 
         public static void LogError(string filename, Exception ex, dynamic obj)
         {
-            // You could use any logging approach here
-
-            StringBuilder builder = new StringBuilder();
-            builder
-                .AppendLine("----------")
-                .AppendLine(DateTime.Now.ToString())
-                .AppendFormat("Source:\t{0}", ex.Source)
-                .AppendLine()
-                .AppendFormat("Target:\t{0}", ex.TargetSite)
-                .AppendLine()
-                .AppendFormat("Type:\t{0}", ex.GetType().Name)
-                .AppendLine()
-                .AppendFormat("Message:\t{0}", ex.Message)
-                .AppendLine()
-                .AppendFormat("Stack:\t{0}", ex.StackTrace)
-                .AppendLine()
-                .AppendFormat("To email:\t{0}", obj.Email)
-                .AppendLine()
-                .AppendFormat("UserName:\t{0}", obj.UserName)
-                .AppendLine()
-                .AppendFormat("Password:\t{0}", obj.Password)
-                .AppendLine()
-                .AppendFormat("ConfirmationToken:\t{0}", obj.ConfirmationToken)
-                .AppendLine();
-
-            string filePath = CustomLog.LogPath + "Log.txt";
-            using (StreamWriter writer = File.AppendText(filePath))
+            lock (locker)
             {
-                writer.Write(builder.ToString());
-                writer.Flush();
+                // You could use any logging approach here
+
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("Source:\t{0}", ex.Source)
+                    .AppendLine()
+                    .AppendFormat("Target:\t{0}", ex.TargetSite)
+                    .AppendLine()
+                    .AppendFormat("Type:\t{0}", ex.GetType().Name)
+                    .AppendLine()
+                    .AppendFormat("Message:\t{0}", ex.Message)
+                    .AppendLine()
+                    .AppendFormat("Stack:\t{0}", ex.StackTrace)
+                    .AppendLine()
+                    .AppendFormat("To email:\t{0}", obj.Email)
+                    .AppendLine()
+                    .AppendFormat("UserName:\t{0}", obj.UserName)
+                    .AppendLine()
+                    .AppendFormat("Password:\t{0}", obj.Password)
+                    .AppendLine()
+                    .AppendFormat("ConfirmationToken:\t{0}", obj.ConfirmationToken)
+                    .AppendLine();
+
+                string filePath = CustomLog.LogPath + "Log.txt";
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
             }
         }
 
         public static void LogError(string filename, Exception ex)
         {
             // You could use any logging approach here
-
-            StringBuilder builder = new StringBuilder();
-            builder
-                .AppendLine("----------")
-                .AppendLine(DateTime.Now.ToString())
-                .AppendFormat("Source:\t{0}", ex.Source)
-                .AppendLine()
-                .AppendFormat("Target:\t{0}", ex.TargetSite)
-                .AppendLine()
-                .AppendFormat("Type:\t{0}", ex.GetType().Name)
-                .AppendLine()
-                .AppendFormat("Message:\t{0}", ex.Message)
-                .AppendLine()
-                .AppendFormat("Stack:\t{0}", ex.StackTrace)
-                .AppendLine();
-
-            string filePath = CustomLog.LogPath + "Log.txt";
-            using (StreamWriter writer = File.AppendText(filePath))
+            lock (locker)
             {
-                writer.Write(builder.ToString());
-                writer.Flush();
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("Source:\t{0}", ex.Source)
+                    .AppendLine()
+                    .AppendFormat("Target:\t{0}", ex.TargetSite)
+                    .AppendLine()
+                    .AppendFormat("Type:\t{0}", ex.GetType().Name)
+                    .AppendLine()
+                    .AppendFormat("Message:\t{0}", ex.Message)
+                    .AppendLine()
+                    .AppendFormat("Stack:\t{0}", ex.StackTrace)
+                    .AppendLine();
+
+                string filePath = CustomLog.LogPath + "Log.txt";
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
             }
         }
 
         public static void LogError(string filename, string ex)
         {
             // You could use any logging approach here
-
-            StringBuilder builder = new StringBuilder();
-            builder
-                .AppendLine("----------")
-                .AppendLine(DateTime.Now.ToString())
-                .AppendFormat("Message:\t{0}", ex)
-                .AppendLine();
-
-            string filePath = CustomLog.LogPath + "Log.txt";
-            using (StreamWriter writer = File.AppendText(filePath))
+            lock (locker)
             {
-                writer.Write(builder.ToString());
-                writer.Flush();
+                StringBuilder builder = new StringBuilder();
+                builder
+                    .AppendLine("----------")
+                    .AppendLine(DateTime.Now.ToString())
+                    .AppendFormat("Message:\t{0}", ex)
+                    .AppendLine();
+
+                string filePath = CustomLog.LogPath + "Log.txt";
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.Write(builder.ToString());
+                    writer.Flush();
+                }
             }
         }
     }
--- WebManageFridgeMQTT/WebManageFridgeMQTT/Utility/Gateway.cs
+++ WebManageFridgeMQTT/WebManageFridgeMQTT/Utility/Gateway.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Timers;
 using System.Web;
 using System.Web.Script.Serialization;
 using uPLibrary.Networking.M2Mqtt;
@@ -13,6 +16,7 @@ namespace WebManageFridgeMQTT.Utility
     public class Gateway
     {
         public MqttClient client;
+        public Timer TimerTick;
 
         public void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
@@ -32,7 +36,7 @@ namespace WebManageFridgeMQTT.Utility
                         ModelMess modelMess = Helper.ParseMessToModel(e.Message.ToList());
                         CustomLog.LogError(modelMess.WriteByteLog());
                         ThietBiStatusMess TbMess = Helper.ParseMessToValue(modelMess, strThietBiID);
-                        if(!string.IsNullOrEmpty(strThietBiID))
+                        if (!string.IsNullOrEmpty(strThietBiID))
                         {
                             CustomLog.LogError(TbMess.WriteLog());
                             using (DeviceTrackingDataContext Context = new DeviceTrackingDataContext())
@@ -40,7 +44,7 @@ namespace WebManageFridgeMQTT.Utility
                                 //Context.UpdateThieBiSatusMess("7E151BF7-559D-4BCC-B8B5-6F5FFAEB86FD", "", "", "", 0, 1, 0, 0, 0, 0);
                                 Context.UpdateThieBiSatusMess(strThietBiID, TbMess.CommandType, TbMess.CommandId, TbMess.CommandAction, TbMess.Loai, TbMess.StatusMay, TbMess.Time, TbMess.TrangThai, TbMess.Latitude, TbMess.Longitude);
                             }
-                            
+
                         }
                     }
                 }
@@ -50,7 +54,7 @@ namespace WebManageFridgeMQTT.Utility
                 CustomLog.LogError(ex);
                 throw;
             }
-            
+
         }
 
         public void client_MqttMsgUnsubscribed(object sender, MqttMsgUnsubscribedEventArgs e)
@@ -66,6 +70,50 @@ namespace WebManageFridgeMQTT.Utility
             string result = "MqttMsgSubscribed----- ClientID: " + clientID + " --- MessageId: " + e.MessageId + " --- GrantedQoSLevels: " + Encoding.UTF8.GetString(e.GrantedQoSLevels);
             CustomLog.LogError(result);
         }
-    }
 
+        public Gateway()
+        {
+            int interval = 600000;
+            if(!string.IsNullOrEmpty(ConfigurationManager.AppSettings["TimePingInterval"].ToString())){
+                Int32.Parse(ConfigurationManager.AppSettings["TimePingInterval"].ToString());
+            }
+            this.TimerTick = new Timer();
+            this.TimerTick.Interval = interval;
+            this.TimerTick.Elapsed += new ElapsedEventHandler(Time_Elapsed);
+            this.TimerTick.AutoReset = true;
+        }
+        public void Time_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                if (this.client.IsConnected)
+                {
+                    byte[] ping = new byte[] { 0x03, 0x01, 0x01 };
+                    this.client.Publish("ping", Encoding.UTF8.GetBytes("ping"));
+                }
+                else
+                {
+                    #region Config
+                    this.client = new MqttClient(IPAddress.Parse("45.117.80.39"));
+                    string clientID = "1111AAAA";
+                    this.client.Connect(clientID);
+                    CustomLog.LogError("reconnect thanh cong");
+                    string[] topic = { "#", "Test/#" };
+
+                    byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE };
+                    this.client.Subscribe(topic, qosLevels);
+
+                    this.client.MqttMsgPublishReceived += this.client_MqttMsgPublishReceived;
+                    this.client.MqttMsgSubscribed += this.client_MqttMsgSubscribed;
+                    this.client.MqttMsgUnsubscribed += this.client_MqttMsgUnsubscribed;
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+                CustomLog.LogError(ex);
+                throw;
+            }
+        }
+    }
 }
